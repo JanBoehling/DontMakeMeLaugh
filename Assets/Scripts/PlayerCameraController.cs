1f4538aa1971b46fb81bfea2634940a06197078f
JanBoehling <janrasmus03.boehling@t-online.de>
2024-01-27 00:42:55 +0100

    Added player controller scripts; added testing scene with testing model of arm

--- Assets/Scripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerCameraController.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class PlayerCameraController : MonoBehaviour
 {
-    [SerializeField] private float cameraSensitivity = 100f;
+    [SerializeField] private float sensitivity = 1f;
+    [SerializeField, Range(0f, 1f)] private float springBackSpeed = .6f;
 
-    [SerializeField] private float maxAngleX = 10f;
-    [SerializeField] private float maxAngleY = 10f;
+    [SerializeField] private Vector2 maxAngle = new(60f, 60f);
 
     private float xRot = default;
     private float yRot = default;
 
     private Transform mainCam = null;
+
+    private float smoothDampVelocityX = default;
+    private float smoothDampVelocityY = default;
 
     private void Awake()
     {
@@ -31,16 +34,20 @@ public class PlayerCameraController : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
-        yRot += mouseX * cameraSensitivity;
-        xRot -= mouseY * cameraSensitivity;
+        yRot += mouseX * sensitivity;
+        xRot -= mouseY * sensitivity;
 
-        xRot = Mathf.Clamp(xRot, -maxAngleX, maxAngleX);
-        yRot = Mathf.Clamp(yRot, -maxAngleY, maxAngleY);
+        //if (Input.GetMouseButton(0))
+        xRot = Mathf.SmoothDamp(xRot, 0f, ref smoothDampVelocityX, 1 - springBackSpeed);
+        yRot = Mathf.SmoothDamp(yRot, 0f, ref smoothDampVelocityY, 1 - springBackSpeed);
+
+        xRot = Mathf.Clamp(xRot, -maxAngle.x, maxAngle.x);
+        yRot = Mathf.Clamp(yRot, -maxAngle.y, maxAngle.y);
     }
 
     private void Rotate()
     {
-        mainCam.localRotation = Quaternion.Euler(xRot, yRot, 0);
+        mainCam.localRotation = Quaternion.Euler(xRot, yRot, 0f);
     }
 
 }
--- Assets/Scripts/PlayerInteractionController.cs
+++ Assets/Scripts/PlayerInteractionController.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class PlayerInteractionController : MonoBehaviour
 {
     [SerializeField] private float interactionRange = 10f;
+    [SerializeField] private LayerMask interactionMask = -1;
+
+    private Camera mainCam = null;
+
+    private void Awake() => mainCam = Camera.main;
 
     private void Update()
     {
@@ -11,6 +16,13 @@ public class PlayerInteractionController : MonoBehaviour
 
     private void Interact()
     {
+        var ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        bool hasIntersect = Physics.Raycast(ray, out var hitInfo, interactionRange, interactionMask);
 
+        Debug.DrawRay(ray.origin, ray.direction * interactionRange, Color.red, duration: 2f);
+
+        if (!hasIntersect || !hitInfo.collider) return;
+
+        if (hitInfo.transform.TryGetComponent<IInteractable>(out var interactable)) interactable.Interact();
     }
 }
